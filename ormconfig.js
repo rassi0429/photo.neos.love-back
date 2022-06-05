20cc89a20ba0bcff761c69ee3e2679962ac765b7
rassi0429 <taitokokoa+rassi@gmail.com>
2022-06-05 19:32:03 +0900

    fix: add port

--- ormconfig.js
+++ ormconfig.js
@@ -1,7 +1,7 @@
 module.exports = {
   type: 'mysql',
   host: process.env.DB_HOST || 'localhost',
-  port: 3388,
+  port: Number(process.env.DB_PORT) || 3388,
   username: 'neos',
   password: 'password',
   database: 'photo',
